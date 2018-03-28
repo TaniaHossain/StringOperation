2dffe6f19ad27b910649b0725313f76304dce668
Tania Hossain <tania_hossain07@yahoo.com>
2018-03-28 12:16:00 +0600

    Init

---
+++ src/StringOperation.java
@@ -0,0 +1,17 @@
+
+public class StringOperation {
+
+	public static void main(String[] args) {
+
+		System.out.println("The Text is ---");
+		
+		String string1 = "Name is ";
+	    System.out.println("My " + string1 + "Tania Hossain.");
+	      
+		String palindrome = "My Name is Tania Hossain.";
+	    int len = palindrome.length();
+	    System.out.println( "And The String Length is : " + len + "." );
+	      
+	}
+
+}
